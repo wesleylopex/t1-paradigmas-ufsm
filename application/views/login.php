4f3648345b444c94a1688c287dc157266d0d30d3
wesleylopex <x.wesleylopes@gmail.com>
2022-05-30 15:44:11 -0300

    fix: import correct files

--- application/views/login.php
+++ application/views/login.php
@@ -1,18 +1,7 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-  <link rel="stylesheet" href="<?= base_url('assets/website/styles/tailwindcss/tailwind.css') ?>">
-  <link rel="stylesheet" href="<?= base_url('assets/website/styles/index.css') ?>">
-  <link rel="stylesheet" href="<?= base_url('assets/website/styles/bootstrap-notify/index.css') ?>">
-
-  <link rel="stylesheet" href="<?= base_url('assets/website/styles/font-awesome/styles/all.min.css') ?>">
-  <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
-
-  <!-- Google Fonts -->
-
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200;300;400;600;700;900&display=swap" rel="stylesheet"> 
+  <?php $this->load->view('imports/website-head.php', $this->data) ?>
 </head>
 <body class="overflow-x-hidden bg-gray-100">
   <main class="w-screen min-h-screen grid place-items-center px-2 md:px-0">
@@ -56,6 +45,6 @@
     </div>
   </main>
 
-  <?php $this->load->view('imports/scripts.php', $this->data) ?> 
+  <?php $this->load->view('imports/website-scripts.php', $this->data) ?>
 </body>
 </html>
